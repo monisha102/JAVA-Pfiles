a5aadc92c59edbc991472ca4fbcbf0655eeb9a88
Monisha Munir <43092912+monisha102@users.noreply.github.com>
2022-10-30 21:24:30 +0600

    Add files via upload

---
+++ GraphArraylist.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Graph{
+
+    Arraylist<Arraylist<Integer>> graph;
+    int V;
+
+    Graph(int nodes){
+
+        V=nodes;
+        graph=new ArrayList<ArrayList<Integer>>();
+
+        for(int i;i<V;i++){
+            graph.add(new Arraylist<Integer>());
+        }
+    }
+
+    void Edge(int v, int u){
+
+        graph.get(v).add(u);
+        graph.get(u).add(v);
+    }
+
+    void printGraph(){
+
+        for(int i;i<V;i++){
+            System.out.print("Node "+i);
+                for(int j : graph.get(i))System.out.print(" ->"+j);
+                    System.out.println();
+                
+        }
+    }
+
+
+}
+
+public class Graph1{
+
+    public static void main(String[] args) {
+
+        Graph g = new Graph(5);
+        g.Edge(1, 2);
+        g.Edge(2, 4);
+        g.Edge(1, 3);
+        g.Edge(4, 3);
+        g.Edge(3, 2);
+
+        g.printGraph();
+        
+    }
+}
