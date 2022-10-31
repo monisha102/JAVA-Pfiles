cd355632f2f22c169c4807e2d761e65d96a06982
Monisha Munir <43092912+monisha102@users.noreply.github.com>
2022-10-31 12:26:34 +0600

    Add files via upload

--- GraphArraylist.java
+++ GraphArraylist.java
@@ -1,51 +1,52 @@
 import java.util.*;
 
-class Graph{
 
-    Arraylist<Arraylist<Integer>> graph;
-    int V;
+class Graph {
 
-    Graph(int nodes){
+  ArrayList<ArrayList<Integer>> graph; 
+  int V;
 
-        V=nodes;
-        graph=new ArrayList<ArrayList<Integer>>();
+  Graph(int nodes) {
 
-        for(int i;i<V;i++){
-            graph.add(new Arraylist<Integer>());
-        }
+    V = nodes;
+    graph = new ArrayList<ArrayList<Integer>>();
+
+    for (int i = 0; i < V; i++) {
+      graph.add(new ArrayList<Integer>()); 
     }
+  }
 
-    void Edge(int v, int u){
+  void Edge(int v, int u) {
 
-        graph.get(v).add(u);
-        graph.get(u).add(v);
+    graph.get(v).add(u);
+    graph.get(u).add(v);
+  }
+
+  void printGraph() {
+
+    for (int i = 0; i < V; i++) { 
+      System.out.print("Node " + i);
+      for (int j : graph.get(i))
+        System.out.print(" ->" + j);
+      System.out.println();
+
     }
-
-    void printGraph(){
-
-        for(int i;i<V;i++){
-            System.out.print("Node "+i);
-                for(int j : graph.get(i))System.out.print(" ->"+j);
-                    System.out.println();
-                
-        }
-    }
-
+  }
 
 }
 
-public class Graph1{
+public class Graph1 {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Graph g = new Graph(5);
-        g.Edge(1, 2);
-        g.Edge(2, 4);
-        g.Edge(1, 3);
-        g.Edge(4, 3);
-        g.Edge(3, 2);
+    Graph g = new Graph(5);
+    g.Edge(0, 2);
+    g.Edge(2, 4);
+    g.Edge(1, 3);
+    g.Edge(4, 3);
+    g.Edge(3, 2);
 
-        g.printGraph();
-        
-    }
+    g.printGraph();
+
+  }
 }
